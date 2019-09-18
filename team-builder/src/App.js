2bc49c112cb131b74f6a1805b0034220ddacbb4c
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2019-09-18 13:09:23 -0700

    Functioning Form

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Member from './component/Member';
+import MemberForm from './component/MemberForm';
+// import Members from './component/Member';
+
 function App() {
+  const [members, setMembers] = useState([]);
+  const addNewMember = member => {
+    const newMember ={
+      id: Date.now(),
+      fullname: member.fullname,
+      email: member.email,
+      roll: member.roll
+    };
+    setMembers([...members, newMember])
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,7 +34,13 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className='MForms'>
+        <h1>Members</h1>
+        <MemberForm addNewMember={addNewMember}/>
+        <Member members={members}/>
+      </div>
     </div>
+    
   );
 }
 
